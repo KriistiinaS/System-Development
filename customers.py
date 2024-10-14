b37d2d89ebcb11de743b4a04e1e2f9dd37b08746
KriistiinaS <160121449+KriistiinaS@users.noreply.github.com>
2024-10-14 13:24:47 +0200

    Update customers.py

--- customers.py
+++ customers.py
@@ -1,3 +1,19 @@
 from flask import Blueprint, request, jsonify
 
 customers_blueprint = Blueprint('customers', __name__)
+
+# List to keep track of customers, used in customer ID
+customer = []
+
+# Create customer
+@customers_blueprint.route('/create-customer', methods=['POST'])
+def create_customer():
+    customer_data = request.get_json()
+    customer_id = len(customer) + 1
+    new_customer = {
+        "age": customer_age,
+        "name": customer_data.get('name'),
+        "address": customer_data.get('address'),
+    }
+    customer.append(new_customer)
+    return jsonify({"message": "Customer added", "customer": new_customer}), 201
