ced56c709394bb477258352f5dfd4002adafc4d7
KriistiinaS <160121449+KriistiinaS@users.noreply.github.com>
2024-10-14 13:11:09 +0200

    Update webapi.py

--- webapi.py
+++ webapi.py
@@ -1,9 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from cars import car_blueprint
+from customers import customer_blueprint
+from employees import employee_blueprint
 
 webapi = Flask(__name__)
 
-# Temporary storage for cars
-cars = []
+# Register blueprints for modular structure
+webapi.register_blueprint(car_blueprint)
+webapi.register_blueprint(customer_blueprint)
+webapi.register_blueprint(employee_blueprint)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 # Read all cars
 @webapi.route('/cars', methods=['GET'])
