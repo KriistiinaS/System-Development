f8f0ab8167d3183cf1dd7aa00fc0c894a1bfae13
KriistiinaS <160121449+KriistiinaS@users.noreply.github.com>
2024-10-14 13:26:45 +0200

    Update customers.py

--- customers.py
+++ customers.py
@@ -17,3 +17,17 @@ def create_customer():
     }
     customer.append(new_customer)
     return jsonify({"message": "Customer added", "customer": new_customer}), 201
+
+# Update customer
+@customer_blueprint.route('/update-custoemr', methods=['PUT'])
+def update_customer(customer_id):
+    customer_data = request.get_json()
+    for i in customer:
+        if i['id'] == customer_id:
+            i['make'] = car_data.get('make', car['make'])
+            i['model'] = car_data.get('model', car['model'])
+            i['year'] = car_data.get('year', car['year'])
+            i['location'] = car_data.get('location', car['location'])
+            i['status'] = car_data.get('status', car['status'])
+            return jsonify({"message": "Car updated", "car": car}), 200
+    return jsonify({"message": "Car not found"}), 404
