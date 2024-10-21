d48f274f8089ad50e5f4b1f0f7eb5e404e45b6d3
KriistiinaS <160121449+KriistiinaS@users.noreply.github.com>
2024-10-21 13:24:00 +0200

    Update user.py

--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 #Code inspired from slide 42 in lecture 7
 from neo4j import GraphDatabase, Driver, AsyncGraphDatabase, AsyncDriver
+import json
 
 URI = "neo4j+s://2c334182.databases.neo4j.io"
 AUTH = ("neo4j", "MB20UmadlTYXGV3DiJB1FQh_B6xgzZfORfSahiSixSk")
@@ -8,17 +9,13 @@ def _get_connection() -> Driver:
   driver = GraphDatabase.driver(URI, auth=AUTH)
   driver.verify_connectivity()
   return driver
+def node_to_json(node):
+  node_properties = dict(node.items())
+  return node_properties
 
-def find_car(car):
-  data = _get_connection().execute_query("MATCH (a:Car) where a.car = $car RETURN a;", car=car)
-  if len(data[0]) > 0:
-    car = Car(car, data[0][0][0]["car"])
-    return car
-  else:
-    return Car(car, "Not found in DB")
-
-#make model year location status
-class Car:
-  def __init__(self, make, model):
-    self.make = make
-    self.model = model
+def findAllCars():
+  with _get_connection().session() as session:
+    cars = session.run("MATCH (a:Car) RETURN a;")
+    nodes_json = (node_to_json(record["a"]) for record in cars]
+    print(nodes_json)
+    return nodes_json
