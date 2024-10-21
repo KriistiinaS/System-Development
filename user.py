ced830da9b3be23b2843c7186fd70583c00d63ba
KriistiinaS <160121449+KriistiinaS@users.noreply.github.com>
2024-10-21 13:17:22 +0200

    Update user.py

--- user.py
+++ user.py
@@ -10,12 +10,12 @@ def _get_connection() -> Driver:
   return driver
 
 def find_car(car):
-  data = _get_connection().execute_query("MATCH (a:User) where a.car = $car RETURN a;", car=car)
+  data = _get_connection().execute_query("MATCH (a:Car) where a.car = $car RETURN a;", car=car)
   if len(data[0]) > 0:
-    user = User(car, data[0][0][0]["car"])
-    return user
+    car = Car(car, data[0][0][0]["car"])
+    return car
   else:
-    return User(car, "Not found in DB")
+    return Car(car, "Not found in DB")
 
 #make model year location status
 class Car:
