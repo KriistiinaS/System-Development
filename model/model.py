dd8f3047c72491fbe38bdf34e6ae9c512e5e921d
julietelnaes <jbtelnaes@gmail.com>
2024-11-07 12:26:44 +0100

    fix book car?

--- model/model.py
+++ model/model.py
@@ -95,7 +95,7 @@ def book_car(customer_id, car_id):
     #Update the car status to 'booked' and create the relationship
     query = """
     MATCH (car:Car {id: $car_id})
-    WHERE car.status = 'available' AND car.condition = 'ok' AND NOT car.condition = 'damaged'
+    WHERE car.status = 'available' AND car.condition <> 'damaged'
     MATCH (customer:Customer {id: $customer_id})
     SET car.status = 'booked'
     MERGE (customer)-[:BOOKED]->(car)
