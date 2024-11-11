8b2f8a43dc7bcfbacfb1ea5e1f34db6de4da3e4d
NR-4 <natalirodri007@gmail.com>
2024-11-11 13:44:45 +0100

    hei

---
+++ System-Development-Testtesttest/System-Development-Testtesttest/webapi.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, jsonify
+from flask_cors import CORS  # Import CORS
+from routes.cars import car_blueprint
+from routes.customers import customer_blueprint
+from routes.employees import employee_blueprint
+
+webapi = Flask(__name__)
+CORS(webapi, resources={r"/*": {"origins": "http://127.0.0.1:5500"}})  # Enable CORS 
+
+@webapi.route("/")
+def home():
+    return render_template("view.html")
+
+# Register blueprints for modular structure
+webapi.register_blueprint(car_blueprint)
+webapi.register_blueprint(customer_blueprint)
+webapi.register_blueprint(employee_blueprint)
+
+if __name__ == '__main__':
+    webapi.run(debug=True)
